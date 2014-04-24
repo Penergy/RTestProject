5774fab249ffa4d7fb2262bfcd78eafb3a391075
Penergy <echo.penergy@gmail.com>
2014-04-24 11:09:40 +0200

    my second commit message

---
+++ abc.R
@@ -0,0 +1,11 @@
+##' .. content for \description{} (no empty lines) ..
+##'
+##' .. content for \details{} ..
+##' @title
+##' @param a
+##' @param b
+##' @return
+##' @author Penergy
+stupid_f = function(a, b){
+    a + b
+}
